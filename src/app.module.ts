dcaad5fbde40d26a8b0592cfe2ef4f1e321b0407
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2024-04-23 15:57:00 -0400

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+import { User } from './entites/user.entity';
+
+@Global()
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3308,
+    username: 'root',
+    password: 'root123',
+    database: 'nest_api',
+    entities: [User],
+    synchronize: true,
+  }),
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+
+  }),
+    UserModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
